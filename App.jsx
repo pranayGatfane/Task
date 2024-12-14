9daf2b7929e2b8621abbc691f522449ddaa4708e
Pranay Surendra Gatfane <pranaygatfane21@gmail.com>
2024-12-14 20:51:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header"
+import { Card } from "./components/Card"
+import { Footer } from "./components/Footer"
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
